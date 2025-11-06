2899b9b668dca5bcaf444a403ccde50fbfdfd3ee
abdelhak122 <48642097+abdelhak122@users.noreply.github.com>
2025-11-06 21:35:13 +0100

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -19,7 +19,7 @@ root.render(
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('./service-worker.js')
       .then(registration => {
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
       })
