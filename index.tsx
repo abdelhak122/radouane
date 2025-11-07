6290640dc32b791540889183b5d864047c2b0e9e
abdelhak122 <abdelhak122@gmail.com>
2025-11-07 21:51:57 +0100

    v3

--- index.tsx
+++ index.tsx
@@ -16,15 +16,3 @@ root.render(
     </Suspense>
   </React.StrictMode>
 );
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./service-worker.js')
-      .then(registration => {
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      })
-      .catch(error => {
-        console.log('ServiceWorker registration failed: ', error);
-      });
-  });
-}
